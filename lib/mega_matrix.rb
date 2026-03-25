2a7fd3e566d23cc4f928fd5eb10c61613dd484d4
Morozov Yura <umorozov726@gmail.com>
2026-03-25 20:42:56 +0300

    first commit

---
+++ lib/mega_matrix.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mega_matrix/version"
+
+module MegaMatrix
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mega_matrix/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MegaMatrix
+  VERSION = "0.1.0"
+end
